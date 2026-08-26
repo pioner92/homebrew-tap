59dec3a7eaf715340e3e7daa9b3285d73a8a6bd1
Alex Shumihin <53976385+pioner92@users.noreply.github.com>
2026-08-26 13:44:20 +0200

    metalterm: 0.1.7

--- Casks/metalterm.rb
+++ Casks/metalterm.rb
@@ -1,6 +1,6 @@
 cask "metalterm" do
-  version "0.1.6"
-  sha256 "ac9017e638cc837fe9448e6591ca760580be4ffe3f93f671af709afb684f4ca9"
+  version "0.1.7"
+  sha256 "ac439fb0801ce73cd72dba57dc5bd37127cc488734bdf4da69142c9196373c71"
 
   url "https://github.com/pioner92/metalterm-site/releases/download/downloads/Metalterm-#{version}.dmg",
       verified: "github.com/pioner92/metalterm-site/"
